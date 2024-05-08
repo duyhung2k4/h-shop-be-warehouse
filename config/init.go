a5367820056c6eaec99052edc238a1654f577262
nguyenduyhung04092004 <nguyenduyhung04092004@gmail.com>
2024-05-09 00:11:36 +0700

    s

--- config/init.go
+++ config/init.go
@@ -8,10 +8,13 @@ import (
 )
 
 func init() {
+	loadEnv()
+
 	var migrate bool = false
 	flag.BoolVar(&migrate, "db", true, "Migrate Database?")
+	flag.StringVar(&dbHost, "host", "localhost", "")
+	flag.Parse()
 
-	loadEnv()
 	jwt = jwtauth.New("HS256", []byte("h-shop"), nil)
 
 	if err := connectPostgresql(migrate); err != nil {
